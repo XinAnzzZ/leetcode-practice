4fdaaaeb5610e62dc807a36fd445e6197ae96134
Moore.Ma <1150989249@qq.com>
2020-06-22 19:18:55 +0800

    Question 54 螺旋矩阵

---
+++ src/com/yuhangma/leetcode/practice/G_array/Q54_SpiralMatrix.java
@@ -0,0 +1,59 @@
+//给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。 
+//
+// 示例 1: 
+//
+// 输入:
+//[
+// [ 1, 2, 3 ],
+// [ 4, 5, 6 ],
+// [ 7, 8, 9 ]
+//]
+//输出: [1,2,3,6,9,8,7,4,5]
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//[
+//  [1, 2, 3, 4],
+//  [5, 6, 7, 8],
+//  [9,10,11,12]
+//]
+//输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+// 
+// Related Topics 数组
+
+
+package com.yuhangma.leetcode.practice.G_array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Moore.Ma
+ * @since 2020/06/22
+ */
+public class Q54_SpiralMatrix {
+    public static void main(String[] args) {
+        Solution solution = new Q54_SpiralMatrix().new Solution();
+
+        testCase();
+    }
+
+    private static void testCase() {
+        Solution solution = new Q54_SpiralMatrix().new Solution();
+    }
+
+    class Solution {
+        public List<Integer> spiralOrder(int[][] matrix) {
+            if (matrix.length == 0) {
+                return new ArrayList<>();
+            }
+            int m = matrix[0].length;
+            int n = matrix.length;
+
+            return null;
+        }
+    }
+
+}
