f4553f31ceeb75a643d22bdcf6cf36a83491eedf
Moore.Ma <1150989249@qq.com>
2020-07-11 22:12:51 +0800

    question 34. 在排序数组中查找元素的第一个和最后一个位置

--- src/com/yuhangma/leetcode/practice/K_DividAndConquer/Q34_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/com/yuhangma/leetcode/practice/K_DividAndConquer/Q34_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -18,6 +18,8 @@
 
 package com.yuhangma.leetcode.practice.K_DividAndConquer;
 
+import java.util.Arrays;
+
 /**
  * @author Moore.Ma
  * @since 2020/07/11
@@ -25,6 +27,8 @@ package com.yuhangma.leetcode.practice.K_DividAndConquer;
 public class Q34_FindFirstAndLastPositionOfElementInSortedArray {
     public static void main(String[] args) {
         Solution solution = new Q34_FindFirstAndLastPositionOfElementInSortedArray().new Solution();
+        int[] arr = {5, 7, 7, 8, 8, 10};
+        System.out.println(Arrays.toString(solution.searchRange(arr, 8)));
         testCase();
     }
 
@@ -34,7 +38,53 @@ public class Q34_FindFirstAndLastPositionOfElementInSortedArray {
 
     class Solution {
         public int[] searchRange(int[] nums, int target) {
-            return null;
+            int[] res = {-1, -1};
+            if (nums == null || nums.length == 0) {
+                return res;
+            }
+            int start = 0;
+            int end = nums.length - 1;
+            int firstPosition = -1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] < target) {
+                    start = mid + 1;
+                } else if (nums[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    for (int i = start; i <= mid; i++) {
+                        if (nums[i] == target) {
+                            firstPosition = i;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            if (firstPosition == -1) {
+                return res;
+            }
+
+            start = 0;
+            end = nums.length - 1;
+            int lastPosition = firstPosition;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] < target) {
+                    start = mid + 1;
+                } else if (nums[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    for (int i = end; i >= mid; i--) {
+                        if (nums[i] == target) {
+                            lastPosition = i;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            return new int[]{firstPosition, lastPosition};
         }
     }
 
