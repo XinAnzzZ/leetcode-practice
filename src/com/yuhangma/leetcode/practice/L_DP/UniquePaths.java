ab0ee61ccb6c80c26da5cf5506275405d20829f5
Moore.Ma <1150989249@qq.com>
2020-07-15 22:52:41 +0800

    question 62. 不同路径

--- src/com/yuhangma/leetcode/practice/L_DP/UniquePaths.java
+++ src/com/yuhangma/leetcode/practice/L_DP/UniquePaths.java
@@ -46,6 +46,7 @@ package com.yuhangma.leetcode.practice.L_DP;
 public class UniquePaths {
     public static void main(String[] args) {
         Solution solution = new UniquePaths().new Solution();
+        System.out.println(solution.uniquePaths(7, 3));
         testCase();
     }
 
@@ -55,7 +56,19 @@ public class UniquePaths {
 
     class Solution {
         public int uniquePaths(int m, int n) {
-
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 0; i < n; i++) {
+                dp[0][i] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+            return dp[m - 1][n - 1];
         }
     }
 
